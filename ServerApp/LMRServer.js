ef535d6dc308cf24115e97caa136140b385dc503
Tobias Haber <t.haber@division.ag>
2016-01-20 14:30:42 +0100

    [TASK] Master Change: Remove Hyperion

--- ServerApp/LMRServer.js
+++ ServerApp/LMRServer.js
@@ -1,4 +1,6 @@
 // IMPORTS
+
+
 "use strict";
 
 var Hyperion = require('hyperion-client');
@@ -6,35 +8,76 @@ var SunCalc = require('./suncalc');
 var t = require('tap');
 var app = require('http').createServer();
 var io = require('socket.io')(app);
+var ledController = require("rpi-ws2801");
+var colorLight = function(c,n,i,d){for(i=3;i--;c[i]=d<0?0:d>255?255:d|0)d=c[i]+n;return c}
+var Gradient = require('gradient');
 
 // VARs
 var currentColor = {"r":0,"g":0,"b":0};
-var defaultColor = {"r":255,"g":255,"b":255};
+//var defaultColor = {"r":255,"g":255,"b":255};
+var defaultColor = [255,255,255];
 var darkTimer,lightTimer,isDark,isLight;
 var firstRun = false;
 var maxLED = 192;
+ledController.connect(maxLED);
+//ledController.gamma(2.1000,2.0000,2.0000);
 var interval;
 var toggle = true;
-var hyperion;
+var lat = 49.5121;
+var lng = 8.5316;
+var tempInterval2;
+var colorChange = false;
+
+// EXIT H&&LER
+process.on( 'SIGINT', function() {
+  ledController.clear(); 
+  ledController.disconnect();
+  process.exit( )
+})
+
+// LED CONNECTION
+var hyperion = new Hyperion('127.0.0.1', 19444 );
+hyperion.on('connect', function(){});
+hyperion.on('error', function(error){});
+
+var times = SunCalc.getTimes(new Date(), lat, lng);
+var sunset = times.sunset;
+/*
+var color = Color().rgb(255, 255, 255);
+for (var i = 0; i <= 100; i++) {
+	var temp = parseFloat(i /100);
+	console.log(temp);
+	console.log(color.darken(temp).red());
+	console.log(color.darken(temp).green());
+	console.log(color.darken(temp).blue());
+};
 
 
+var clockTimer = setInterval(function(){
+	if(sunset.getTime() <= new Date().getTime()){
+		console.log("hallo");
+	}else{
+		console.log('No');
+	}
+},60000);
+*/
+
+
+console.log(times.sunset);
+
+// APP CONNECTION
 io.on('connection', function (socket) {
-	LMRemote.firstConnect();
 	socket.emit('connected', { hello: 'world' });
 	socket.on('color', function (data) {
-		console.log(data);
 		LMRemote.color(data);
 	});
 	socket.on('effect', function (data) {
-		console.log(data);
 		LMRemote.effect(data);
 	});
 	socket.on('auto', function (data) {
-		console.log(data);
 		LMRemote.automation(data);
 	});
 	socket.on('timer', function (data) {
-		console.log(data);
 	});
 	socket.on('defaultColor', function () {
 		LMRemote.setDefaultColor();
@@ -52,25 +95,20 @@ app.listen(5555);
 
 // BASE FUNCTIONS
 var LMRemote = {
-	firstConnect : function(){
-		if(!firstRun){
-			while(!firstRun){
-				hyperion = new Hyperion('127.0.0.1', 19444 );
-				hyperion.on('connect', function(){
-					firstRun = true;
-					hyperion.setColor([ 255, 0, 0,0,255,0,0,0,255], function( err, result ){});
-				});
-				hyperion.on('error', function(error){
-					console.error('error:', error)
-				});
-			}
-		}
-	},
 	color : function(rgbColor){
 			currentColor = rgbColor;
-		    hyperion.setColor([currentColor.r,currentColor.g,currentColor.b], function( err, result ){});
+			if(!colorChange){
+				this.clearAll();
+				colorChange = true;
+				this.sendColor(false,[rgbColor.r,rgbColor.g,rgbColor.b], function(result){
+					LMRemote.sendColor(false,[currentColor.r,currentColor.g,currentColor.b], function(){});
+					colorChange = false;
+				});
+			}
 	},
 	effect : function(data){
+		this.clearAll();
+		this.clearColor
 		switch(data.name){
 			case "clock":
 				Effects.clock();
@@ -96,6 +134,7 @@ var LMRemote = {
 		}
 	},
 	automation : function(data){
+		this.clearAll();
 		switch(data){
 			case "night":
 				Timers.darkness();
@@ -112,18 +151,266 @@ var LMRemote = {
 	clearAll : function(){
 		clearInterval(interval);
 	},
+	clearColor : function(){
+		this.sendColor(false,byteArray, function( err, result ){});
+	},
 	lightOn : function(){
+		this.clearAll();
 		this.color(defaultColor);
 	},
 	setDefaultColor : function(){
 		defaultColor = currentColor;
+	},
+	sendColor : function(num,rgbArray,callback){
+		setTimeout(function(){
+			if(num){
+				var x = 100;
+				var transColor = new Buffer(ledController.getChannelCount());
+				clearInterval(tempInterval2);
+				tempInterval2 = setInterval(function(){
+					var colorBuffer = new Buffer(ledController.getChannelCount());
+					for (var i=0; i<rgbArray.length; i+=3){
+						var temp = blendColors(rgbArray[i],rgbArray[i+1],rgbArray[i+2],defaultColor[i],defaultColor[i+1],defaultColor[i+2],(x/100))
+						transColor[i] = temp.r;
+						transColor[i+1] = temp.g;
+						transColor[i+2] = temp.b;
+					}
+					for (var i=0; i<colorBuffer.length; i+=3){
+						colorBuffer[i] = transColor[i];
+						colorBuffer[i+1] = transColor[i+1];
+						colorBuffer[i+2] = transColor[i+2];
+					}
+					ledController.sendRgbBuffer(colorBuffer);
+					x--;
+					if(x <= 0){
+						clearInterval(tempInterval2);
+						defaultColor = rgbArray;
+						callback(true);
+					} 
+				}, 5);
+			}else{
+				var x = 100;
+				clearInterval(tempInterval2);
+				tempInterval2 = setInterval(function(){
+					var temp = blendColors(rgbArray[0],rgbArray[1],rgbArray[2],defaultColor[0],defaultColor[1],defaultColor[2],(x/100))
+					var colorBuffer = new Buffer(ledController.getChannelCount());
+					for (var i=0; i<colorBuffer.length; i+=3){
+						colorBuffer[i] = temp.r;
+						colorBuffer[i+1] = temp.g;
+						colorBuffer[i+2] = temp.b;
+					}
+					ledController.sendRgbBuffer(colorBuffer);
+					x=x-5;
+					if(x <= 0){
+						clearInterval(tempInterval2);
+						defaultColor = rgbArray;
+						callback(true);
+					} 
+				}, 5);
+			}
+		}, 10);
 	}
+}
+
+function blendColors(r1,g1,b1,r2,g2,b2,balance) {
+    var bal = Math.min(Math.max(balance,0),1);
+    var nbal = 1-bal;
+    return {
+            r : Math.floor(r1*nbal + r2*bal),
+            g : Math.floor(g1*nbal + g2*bal),
+            b : Math.floor(b1*nbal + b2*bal)
+           };
+}   
+
+function rgb2hsv () {
+    var rr, gg, bb,
+        r = arguments[0] / 255,
+        g = arguments[1] / 255,
+        b = arguments[2] / 255,
+        h, s,
+        v = Math.max(r, g, b),
+        diff = v - Math.min(r, g, b),
+        diffc = function(c){
+            return (v - c) / 6 / diff + 1 / 2;
+        };
+
+    if (diff == 0) {
+        h = s = 0;
+    } else {
+        s = diff / v;
+        rr = diffc(r);
+        gg = diffc(g);
+        bb = diffc(b);
+
+        if (r === v) {
+            h = bb - gg;
+        }else if (g === v) {
+            h = (1 / 3) + rr - bb;
+        }else if (b === v) {
+            h = (2 / 3) + gg - rr;
+        }
+        if (h < 0) {
+            h += 1;
+        }else if (h > 1) {
+            h -= 1;
+        }
+    }
+    return {
+        h: Math.round(h * 360),
+        s: Math.round(s * 100),
+        v: Math.round(v * 100)
+    };
+}
+
+function hsvToRgb(h, s, v) {
+	var r, g, b;
+	var i;
+	var f, p, q, t;
+ 
+	// Make sure our arguments stay in-range
+	h = Math.max(0, Math.min(360, h));
+	s = Math.max(0, Math.min(100, s));
+	v = Math.max(0, Math.min(100, v));
+ 
+	// We accept saturation and value arguments from 0 to 100 because that's
+	// how Photoshop represents those values. Internally, however, the
+	// saturation and value are calculated from a range of 0 to 1. We make
+	// That conversion here.
+	s /= 100;
+	v /= 100;
+ 
+	if(s == 0) {
+		// Achromatic (grey)
+		r = g = b = v;
+		return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+	}
+ 
+	h /= 60; // sector 0 to 5
+	i = Math.floor(h);
+	f = h - i; // factorial part of h
+	p = v * (1 - s);
+	q = v * (1 - s * f);
+	t = v * (1 - s * (1 - f));
+ 
+	switch(i) {
+		case 0:
+			r = v;
+			g = t;
+			b = p;
+			break;
+ 
+		case 1:
+			r = q;
+			g = v;
+			b = p;
+			break;
+ 
+		case 2:
+			r = p;
+			g = v;
+			b = t;
+			break;
+ 
+		case 3:
+			r = p;
+			g = q;
+			b = v;
+			break;
+ 
+		case 4:
+			r = t;
+			g = p;
+			b = v;
+			break;
+ 
+		default: // case 5:
+			r = v;
+			g = p;
+			b = q;
+	}
+ 
+	return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function hex (c) {
+  var s = "0123456789abcdef";
+  var i = parseInt (c);
+  if (i == 0 || isNaN (c))
+    return "00";
+  i = Math.round (Math.min (Math.max (0, i), 255));
+  return s.charAt ((i - i % 16) / 16) + s.charAt (i % 16);
+}
+
+/* Convert an RGB triplet to a hex string */
+function convertToHex (rgb) {
+  return hex(rgb[0]) + hex(rgb[1]) + hex(rgb[2]);
+}
+
+
+function trim (s) { return (s.charAt(0) == '#') ? s.substring(1, 7) : s }
+
+function convertToRGB (hex) {
+  var color = [];
+  color[0] = parseInt ((trim(hex)).substring (0, 2), 16);
+  color[1] = parseInt ((trim(hex)).substring (2, 4), 16);
+  color[2] = parseInt ((trim(hex)).substring (4, 6), 16);
+  return color;
+}
+
+function generateColor(colorStart,colorEnd,colorCount){
+
+	// The beginning of your gradient
+	var start = convertToRGB (colorStart);    
+
+	// The end of your gradient
+	var end   = convertToRGB (colorEnd);    
+
+	// The number of colors to compute
+	var len = colorCount;
+
+	//Alpha blending amount
+	var alpha = 1.0;
+
+	var saida = [];
+	
+	for (var i = 0; i < len; i++) {
+		var c = [];
+		alpha += (1.0/len);
+		
+		c[0] = start[0] * alpha + (1 - alpha) * end[0];
+		c[1] = start[1] * alpha + (1 - alpha) * end[1];
+		c[2] = start[2] * alpha + (1 - alpha) * end[2];
+
+		saida.push(parseInt(convertToRGB(convertToHex (c)).toString()));
+		
+	}
+	
+	return saida;
+	
 }
 
 // EFFECT FUNCTIONS
 var Effects = {
 	rainbow : function(){
-			hyperion.setEffect('Rainbow swirl', {}, function( err, result ){});
+		var colorBuffer = new Buffer(ledController.getChannelCount());
+		var animationTick = 0.005;
+		var angle = 0;
+		var ledDistance = 0.3;
+		var brightness = 1.0;
+		var saturation = 1.0;
+		interval = setInterval(function(){
+			var ledData = Array()
+			for (var i = 0; i < maxLED; i++) {		
+				for (var i = 0; i < maxLED; i++) {		
+					var hue = i/maxLED;
+					var rgb = hsvToRgb(hue, saturation, brightness);
+					ledData.push(rgb[0]);
+					ledData.push(rgb[1]);
+					ledData.push(rgb[2]);
+				};
+				ledController.sendRgbBuffer(ledData);
+			};
+		},5);
 	},
 	clock : function(){
 		var leds = maxLED / 60;
@@ -134,9 +421,9 @@ var Effects = {
 				current = 0;
 			}
 			for (var i = current; i >= 0; i--) {
-				byteArray.push(currentRed) // R
-				byteArray.push(currentGreen) // G
-				byteArray.push(currentBlue) // B      
+				byteArray.push(currentColor.r) // R
+				byteArray.push(currentColor.g) // G
+				byteArray.push(currentColor.b) // B      
 			};
 			for (var i = maxLED-leds; i >= 0; i--) {
 				byteArray.push(0) // R
@@ -144,11 +431,65 @@ var Effects = {
 				byteArray.push(0) // B
 			};
 			current = current+leds;
-			hyperion.setColor(byteArray, function( err, result ){});
+			LMRemote.sendColor(false,byteArray, function( err, result ){});
 		},1000);
 	},
 	mood : function(){
-		hyperion.setEffect('Cold mood blobs',{}, function( err, result ){});
+		var amplitudePhase = 0.0;
+		var rotateColors = false;
+		var baseColorChangeStepCount = 0;
+		var baseHSVValue = 1;
+		var tempArray = Array();
+		var tempColor = Array();
+		for (var i = maxLED; i >= 0; i--) {
+			tempColor.push(0);
+			tempColor.push(0);
+			tempColor.push(255);
+		}
+		var hueChange = 60;
+		var numberOfRotates = 0;
+		var baseColorChangeRate = 40;
+		var baseColorRangeLeft = 0.0;
+		var baseColorRangeRight = 360.0;
+		var baseColorChangeIncreaseValue = 1.0 / 360.0;
+		var blobs = 5;
+		var colors = Array(maxLED);
+		var hsv = rgb2hsv(0,0,255);
+		var baseHSVValue = hsv.h;
+		var amplitudePhaseIncrement = blobs * Math.PI * 0.1 / 20.0;
+		var grad = Gradient('#303F9F', '#3F51B5', '#009688','#E91E63','#8BC34A','#2196F3', maxLED);
+		var tempRGB = grad.toArray('rgbArray');
+		var merged = [].concat.apply([], tempRGB);
+		tempColor = merged;
+		interval = setInterval(function(){
+					if(baseColorChangeStepCount >= baseColorChangeRate){
+						baseColorChangeStepCount = 0;
+						tempColor = [];
+		                if(baseColorChangeIncreaseValue < 0 && baseHSVValue > baseColorRangeLeft && (baseHSVValue + baseColorChangeIncreaseValue) <= baseColorRangeLeft){
+		                    baseColorChangeIncreaseValue = abs(baseColorChangeIncreaseValue);
+		                }else if(baseColorChangeIncreaseValue > 0 && baseHSVValue < baseColorRangeRight && (baseHSVValue + baseColorChangeIncreaseValue)  >= baseColorRangeRight){
+                    		baseColorChangeIncreaseValue = -Math.abs(baseColorChangeIncreaseValue);
+		                }
+                	baseHSVValue = (baseHSVValue + baseColorChangeIncreaseValue) % 1.0;
+                	for (var i = maxLED; i >= 0; i--) {
+							var hue = (baseHSVValue + hueChange * Math.sin(2*Math.PI * i / maxLED)) % 1;
+							var rgb = hsvToRgb(hue, hsv.s, hsv.v);
+							tempColor.push(rgb[0]);
+							tempColor.push(rgb[1]);
+							tempColor.push(rgb[2]);
+						}
+					}
+					for (var i = maxLED; i >= 0; i--) {
+						var amplitude = Math.sin(-amplitudePhase + 2*Math.PI * blobs * i / maxLED);
+						colors[3*i+0] = (parseInt(tempColor[3*i+0] * amplitude));
+						colors[3*i+1] = (parseInt(tempColor[3*i+1] * amplitude));
+						colors[3*i+2] = (parseInt(tempColor[3*i+2] * amplitude));
+					}
+					ledController.sendRgbBuffer(colors);
+					amplitudePhase = (amplitudePhase + amplitudePhaseIncrement) % (2*Math.PI);
+					tempArray = tempColor;
+					//baseColorChangeStepCount++;
+		},8);
 	},
 	notify : function(color){
 		var half = maxLED / 2;
@@ -171,28 +512,34 @@ var Effects = {
 				}
 			};
 			toggle = !toggle;
-			hyperion.setColor(byteArray, function( err, result ){});
+			LMRemote.sendColor(false,byteArray, function( err, result ){});
     	}, 2000);
 	},
 	sunrise : function(){
 
 	},
 	nightsky : function(){
+		var tempArray = Array();
+		for (var i = maxLED; i >= 0; i--) {
+			tempArray.push(41);
+			tempArray.push(86);
+			tempArray.push(143);
+		}
+		LMRemote.sendColor(true,tempArray,function(){});
 		interval = setInterval(function() {
 			var byteArray = Array();
 			for (var i = maxLED; i >= 0; i--) {
 				if(Tools.randomBoolean()){
-					byteArray.push(41) // R
-					byteArray.push(86) // G
-					byteArray.push(143) // B
+					byteArray.push(41);
+					byteArray.push(86);
+					byteArray.push(143);
 				}else{
-					byteArray.push(255) // R
-					byteArray.push(255) // G
-					byteArray.push(255) // B 
-				}
+					byteArray.push(255);
+					byteArray.push(255);
+					byteArray.push(255);				}
 			};
-			hyperion.setColor(byteArray, function( err, result ){});
-		},5000);
+			LMRemote.sendColor(true,byteArray,function(){});
+		},10000);
 	}
 }
 
@@ -240,6 +587,6 @@ var Tools = {
 		return Math.floor(Math.random() * (high - low) + low);
 	},
 	randomBoolean : function(){
-		return Math.random() < 0.90;
+		return Math.random() < 0.96;
 	}
 }
